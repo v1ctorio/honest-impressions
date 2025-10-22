ab91f851c374208976a91c865efeca6ddf3fc668
Vic <vicxd@pm.me>
2025-10-22 17:50:35 +0200

    rich text input and error handling for impressions

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
-import Slack, {App, MessageShortcut, SlackShortcutMiddlewareArgs } from "@slack/bolt";
+import Slack, { App, MessageShortcut, SlackActionMiddlewareArgs, SlackShortcutMiddlewareArgs, webApi } from "@slack/bolt";
 import crypto from "crypto";
-import { Blocks, Elements, Modal } from 'slack-block-builder';
+import { Actions, Blocks, Button, Divider, Elements, Message, Modal } from 'slack-block-builder';
+import { CustomRichText, HashUser, RichText, RichTextInput } from "./utils.js";
 
 
-const { SLACK_SIGNING_SECRET,SLACK_APP_TOKEN, SLACK_BOT_TOKEN, PORT, SALT, BANNED_LIST_LOCATION, REVIEWERS } = process.env;
+const { SLACK_SIGNING_SECRET, SLACK_APP_TOKEN, SLACK_BOT_TOKEN, PORT, SALT, BANNED_LIST_LOCATION, REVIEWERS, REVIEW_CHANNEL_ID, IMPRESSIONS_CHANNEL_ID } = process.env;
 
 const reviewers = REVIEWERS ? REVIEWERS.split(",") : [];
 
@@ -22,7 +23,7 @@ const BuildApp = (): App => {
 
   // Start the app
 
-  app.shortcut("delete_me", async ({ ack, body, say }: SlackShortcutMiddlewareArgs<MessageShortcut>) => {
+  app.shortcut("delete_me", async ({ ack, body, say, }: SlackShortcutMiddlewareArgs<MessageShortcut>) => {
     ack();
     if (
       [
@@ -58,6 +59,26 @@ const BuildApp = (): App => {
 
     console.log(body);
 
+
+    if (body.channel.id !== IMPRESSIONS_CHANNEL_ID) {
+      const errorModal = Modal().title("Error!")
+        .blocks(
+          Blocks.Section().text("Anonymous impressions are only seeked in the honest impressions channel!")
+        )
+        .submit("Ok")
+        .callbackId("view_auto_ok")
+        .buildToObject();
+
+
+
+      await app.client.views.open({
+        trigger_id: body.trigger_id,
+        view: errorModal
+      });
+      return;
+    }
+
+
     let modal = Modal()
       .title("Honest impressions reply")
       .callbackId("impression_id")
@@ -65,8 +86,9 @@ const BuildApp = (): App => {
         Blocks.Input()
           .label("Your honest impression")
           .element(
-            Elements.TextInput().multiline(true).actionId("dreamy_input").placeholder("Honestly I think..."),
+            RichTextInput("dreamy_input", true)
           )
+          .blockId("dreamy_input_block")
       ).submit("Submit")
       .privateMetaData(`${body.message_ts}`);
 
@@ -79,8 +101,55 @@ const BuildApp = (): App => {
     });
   });
 
+  app.view("impression_id", async ({ ack, body, view, client }) => {
+    await ack();
 
+    const hUser = HashUser(body.user.id);
+    const impression_block = view.state.values["dreamy_input_block"]["dreamy_input"] as any;
+    // Sorry for TS. block kit is annoying
+    const impression = impression_block["rich_text_value"]["elements"][0]["elements"] as RichText;
+
+    console.log(body, view);
+
+    console.log(impression, hUser);
+
+  });
+
+
+  app.view("view_auto_ok", async ({ ack }) => {
+    await ack();
+  });
   return app;
 };
 
+
+
+function sendImpressionToReview(impression: RichText, parentTs: string, hashedUser: string, client: webApi.WebClient) {
+  let reviewMessage = Message()
+    .channel(REVIEW_CHANNEL_ID)
+    .text("New honest impression to review, open in the slack app to review.")
+    .blocks(
+      Blocks.Section().text(`New honest impression to review. `),
+      Divider(),
+      CustomRichText(impression),
+      Divider(),
+      Actions()
+        .elements(
+          Button()
+            .text("Approve")
+            .primary()
+            .actionId("approve_impression")
+            .value(parentTs),
+          Button()
+            .text("Delete")
+            .actionId("delete_impression"),
+          Button()
+            .text("Ban user")
+            .danger()
+            .actionId("ban_user")
+            .value(hashedUser),
+        ),
+    );
+    
+}
 export default BuildApp;
--- src/utils.ts
+++ src/utils.ts
@@ -2,13 +2,37 @@ import crypto from "crypto";
 const { SALT, BANNED_LIST_LOCATION } = process.env;
 
 var memoryBanned: Array<string> = [];
+export type RichText = Array<any>;
 
+export function HashUser(userId: string): string {
 
-export function HashUser(userId: string, salt: string): string {
+    const salt = process.env.SALT!!;
+
     return crypto.createHash('sha256').update(userId + salt).digest('hex');
 }
 
 export function IsUserBanned(userHash: string): boolean {
     //TODO: implement file-based banned list
     return memoryBanned.includes(userHash);
+}
+
+// custom RichText element because slack-block-builder doesn't have it yet :pf: 
+// see https://github.com/raycharius/slack-block-builder/issues/133#issuecomment-2090479474
+export function CustomRichText(fields: RichText ): any {
+    return {
+        build: ()=>({
+            type: "rich_text",
+            elements: fields
+        })
+    }
+}
+
+export function RichTextInput(actionId: string, focusOnLoad: boolean = false): any {
+    return {
+        build: ()=>({
+            type: "rich_text_input",
+            action_id: actionId,
+            focus_on_load: focusOnLoad
+        })
+    }
 }
